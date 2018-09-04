77aebe64e51e509d25f1da965fb3e368e916bcec
laetitiaouelle <34172926+laetitiaouelle@users.noreply.github.com>
2018-09-04 06:43:51 +0200

    Add files via upload

---
+++ files/footer.php
@@ -0,0 +1,61 @@
+<footer>
+		<div class="container margin_120_95">
+			<div class="row">
+				<div class="col-lg-5 col-md-12 p-r-5">
+					<p><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></p>
+					<p>Programmer nan, c'est pas sorcier</p>
+					<div class="follow_us">
+						<ul>
+							<li>Suivez Nous</li>
+							<li><a href="#0"><i class="ti-facebook"></i></a></li>
+							<li><a href="#0"><i class="ti-twitter-alt"></i></a></li>
+							<li><a href="#0"><i class="ti-google"></i></a></li>
+							<li><a href="#0"><i class="ti-pinterest"></i></a></li>
+							<li><a href="#0"><i class="ti-instagram"></i></a></li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-lg-3 col-md-6 ml-lg-auto">
+					<h5>Liens Utils</h5>
+					<ul class="links">
+						<li><a href="#0">Admission</a></li>
+						<li><a href="#0">A Propos</a></li>
+						<li><a href="#0">Login</a></li>
+						<li><a href="#0">Register</a></li>
+						<li><a href="#0">Nouvel &amp; Evenement</a></li>
+						<li><a href="#0">Contacts</a></li>
+					</ul>
+				</div>
+				<div class="col-lg-3 col-md-6">
+					<h5>Contactez Nous</h5>
+					<ul class="contacts">
+						<li><a href="tel://61280932400"><i class="ti-mobile"></i> + 61 23 8093 3400</a></li>
+						<li><a href="mailto:info@udema.com"><i class="ti-email"></i> info@udema.com</a></li>
+					</ul>
+					<div id="newsletter">
+					<h6>Newsletter</h6>
+					<div id="message-newsletter"></div>
+					<form method="post" action="assets/newsletter.php" name="newsletter_form" id="newsletter_form">
+						<div class="form-group">
+							<input type="email" name="email_newsletter" id="email_newsletter" class="form-control" placeholder="Your email">
+							<input type="submit" value="Submit" id="submit-newsletter">
+						</div>
+					</form>
+					</div>
+				</div>
+			</div>
+			<!--/row-->
+			<hr>
+			<div class="row">
+				<div class="col-md-8">
+					<ul id="additional_links">
+						<li><a href="#0">Terms and conditions</a></li>
+						<li><a href="#0">Privacy</a></li>
+					</ul>
+				</div>
+				<div class="col-md-4">
+					<div id="copy">© 2018 Ecolenan</div>
+				</div>
+			</div>
+		</div>
+	</footer>
---
+++ files/menu.php
@@ -0,0 +1,58 @@
+<header class="header menu_2">
+		<div id="preloader"><div data-loader="circle-side"></div></div><!-- /Preload -->
+		<div id="logo">
+			<a href="index.php"><img src="img/logo.png" width="149" height="42" data-retina="true" alt=""></a>
+		</div>
+		<ul id="top_menu">
+			<li><a href="login.php" class="login">Login</a></li>
+			<li><a href="#0" class="search-overlay-menu-btn">Search</a></li>
+			<li class="hidden_tablet"><a href="admission.php" class="btn_1 rounded">Admission</a></li>
+		</ul>
+		<!-- /top_menu -->
+		<a href="#menu" class="btn_mobile">
+			<div class="hamburger hamburger--spin" id="hamburger">
+				<div class="hamburger-box">
+					<div class="hamburger-inner"></div>
+				</div>
+			</div>
+		</a>
+		<nav id="menu" class="main-menu">
+			<ul>
+				<li><span><a href="index.php">Acceuil</a></span>
+					
+				</li>
+				<li><span><a href="#0">Cours</a></span>
+					<ul>
+						
+						<li><a href="courses-list.php">Liste des cours</a></li>
+						<li><a href="admission.php">Assistant d'admission</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">More +</a></span>
+					<ul>
+						<li><a href="about.php">About</a></li>
+						<li><a href="blog.php">Blog</a></li>
+						<li><a href="login.php">Login</a></li>
+						<li><a href="register.php">Register</a></li>
+						<li><a href="contacts.php">Contacts</a></li>
+						<li><a href="agenda-calendar.php">Agenda Calendrier</a></li>
+					</ul>
+				</li>
+				<li><span><a href="#0">Media</a></span>
+					<ul>
+						<li><a href="media-gallery.php">Media gallery</a></li>
+						<li><a href="coming_soon/index.php">Arrive bientôt</a></li>
+					</ul>
+				</li>
+			</ul>
+		</nav>
+		<!-- Search Menu -->
+		<div class="search-overlay-menu">
+			<span class="search-overlay-close"><span class="closebt"><i class="ti-close"></i></span></span>
+			<form role="search" id="searchform" method="get">
+				<input value="" name="q" type="search" placeholder="Search..." />
+				<button type="submit"><i class="icon_search"></i>
+				</button>
+			</form>
+		</div><!-- End Search Menu -->
+	</header>
